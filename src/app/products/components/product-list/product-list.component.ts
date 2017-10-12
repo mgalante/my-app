b548a5a8d15c4b632b4f3187aa24b8a5d49b3a59
Miguel Galante <mgalante@gmail.com>
2017-10-12 15:38:07 -0300

    lista

--- src/app/products/components/product-list/product-list.component.ts
+++ src/app/products/components/product-list/product-list.component.ts
@@ -4,7 +4,7 @@ import { Product } from '../../models/product.model';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
 
